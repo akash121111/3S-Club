b29e9cefcc9ddebf11f98553bc204d7bee430b38
amityada <amityadav07051998@gmail.com>
2019-07-05 16:06:26 +0530

    commit

---
+++ app/controllers/owner_controller.rb
@@ -0,0 +1,11 @@
+
+class OwnerController < ApplicationController
+  def index
+  end
+
+  def signup
+  end
+
+  def login
+  end
+end
---
+++ db/migrate/20190704101618_owner_signup.rb
@@ -0,0 +1,8 @@
+class OwnerSignup < ActiveRecord::Migration[5.2]
+  def change
+  	create table :users do |t|
+
+
+
+  end
+end
