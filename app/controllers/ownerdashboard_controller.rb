0a7fb84111ae5384b0136089c41315259b0022ca
MukeshSingh <mukeshdbit13495@gmail.com>
2019-07-09 11:29:15 +0530

    commit

--- app/controllers/ownerdashboard_controller.rb
+++ app/controllers/ownerdashboard_controller.rb
@@ -1,5 +1,10 @@
 class OwnerdashboardController < ApplicationController
   def index
+    @br=BookingRecord.new
+    @space=Space.find_by(@br.space_id)
+    @image=SpaceImage.find_by(@br.space_id)
+    @time=SpaceAvailabilityTiming.find_by(@br.user_id)
+    @day=SpaceAvailableDay.find_by(@br.space_id)
   end
 
   def new
