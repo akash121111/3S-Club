7ff969efe0e7e4e5308f91382913f15e87ded72c
akash121111 <akashkumar121111@gmail.com>
2019-07-13 01:29:54 +0530

    hashes used

--- app/controllers/searchings_controller.rb
+++ app/controllers/searchings_controller.rb
@@ -1,6 +1,6 @@
 class SearchingsController < ApplicationController
     def index
-
+byebug
       if params.has_key?(:date) 
   @a=DateTime.parse(params[:date]).to_date.strftime('%A')
 else 
@@ -9,13 +9,13 @@ end
 
 finddayid ={ "Monday" => "1", "Tuesday" => "2", "Wednesday" => "3" , "Thrusday" => "4", "Friday" => "5", "Saturday" => "6", "Sunday" => "7"}
 
-finddayid[@a]
+@k=finddayid[@a]
     @space_addresses = SpaceAddress.search(params[:search])
     @findsearchaddresspid=@space_addresses.pluck(:space_id)
     @findsearchdatepid=SpaceAvailableDay.searchday(@a)
     @findcommonsearch= @findsearchaddresspid & @findsearchdatepid
     @aftersearchbydayandaddress=SpaceAddress.find(@findcommonsearch)
-    @s=SpaceAddress.find(@findcommonsearch).pluck(:space_id)
+   
     @hash = Gmaps4rails.build_markers(@aftersearchbydayandaddress) do |r, marker|
       marker.lat r.latitude
       marker.lng r.longitude
