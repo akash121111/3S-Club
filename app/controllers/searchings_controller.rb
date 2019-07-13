44ba5bd2d2906e30c2684f29ffd31aa50b48b52b
akash121111 <akashkumar121111@gmail.com>
2019-07-13 14:44:47 +0530

    design map search

--- app/controllers/searchings_controller.rb
+++ app/controllers/searchings_controller.rb
@@ -1,20 +1,31 @@
 class SearchingsController < ApplicationController
     def index
-byebug
+
       if params.has_key?(:date) 
   @a=DateTime.parse(params[:date]).to_date.strftime('%A')
 else 
   date = DateTime.now
 end
 
-finddayid ={ "Monday" => "1", "Tuesday" => "2", "Wednesday" => "3" , "Thrusday" => "4", "Friday" => "5", "Saturday" => "6", "Sunday" => "7"}
+finddayid ={ "Monday" => "1", "Tuesday" => "2", "Wednesday" => "3" , "Thursday" => "4", "Friday" => "5", "Saturday" => "6", "Sunday" => "7"}
 
-@k=finddayid[@a]
+@k=finddayid[@a].to_i
+
+
     @space_addresses = SpaceAddress.search(params[:search])
     @findsearchaddresspid=@space_addresses.pluck(:space_id)
     @findsearchdatepid=SpaceAvailableDay.searchday(@a)
     @findcommonsearch= @findsearchaddresspid & @findsearchdatepid
     @aftersearchbydayandaddress=SpaceAddress.find(@findcommonsearch)
+
+x = @aftersearchbydayandaddress.pluck(:space_id).length
+i=0
+while i < x
+  @p=@aftersearchbydayandaddress.pluck(:space_id)[i]
+  @l=SpaceAvailabilityTiming.se(@p,@k)
+  i=i+1
+end
+
    
     @hash = Gmaps4rails.build_markers(@aftersearchbydayandaddress) do |r, marker|
       marker.lat r.latitude
