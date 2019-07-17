1af7a4a1390fd8b7aabd92de66b5b7f07935c6ed
akash121111 <akashkumar121111@gmail.com>
2019-07-17 11:20:00 +0530

    heruko

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "SharedStudySpace_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.assets.initialize_on_precompile = false
+  config.assets.compile = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
