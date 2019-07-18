c4f7d8f891f25cf9961936ee9eae6ff840443424
Nishant Malik <mnishant645@gmail.com>
2019-07-18 23:31:50 +0530

    dashboard form modified

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -1,4 +1,6 @@
 class UserDetail < ApplicationRecord
     belongs_to:user
+    validates :first_name, :last_name, :education_details,
+    :about_user, :city,:college,:phone_number,:mobile_number, presence: true 
   
 end
