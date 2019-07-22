edb3a9e42d20ad0daf9929f092927bfad47221d5
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2019-07-22 18:14:49 +0530

    Password Updation Changed

--- app/assets/javascripts/user_details.js
+++ app/assets/javascripts/user_details.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     $('#user_email').prop('readonly','true');
-    
-    $('#user_password').prop('readonly','true');
-    $('#user_password_confirmation').prop('readonly','true');
+    $('#user_old_password').focusout(function(){
+        $.ajax({
+                type : "get",
+                data : { data_value: $('#user_old_password').val() }
+        });
+    });
 
-});
+}); 
