64524390fa37ecf03efa92a8fdde5695409d2069
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2019-07-25 10:54:10 +0530

    secret key config for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "SharedStudySpace_#{Rails.env}"
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   config.action_mailer.perform_caching = false
 
