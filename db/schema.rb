3d8acca9ff73c84abdd1055a37fde275d3a82e11
Akash <akashkumar121111@gmail.com>
2019-08-09 18:47:06 +0530

    conversation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_115006) do
+ActiveRecord::Schema.define(version: 2019_08_09_085650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,15 @@ ActiveRecord::Schema.define(version: 2019_08_01_115006) do
     t.index ["user_id"], name: "index_booking_records_on_user_id"
   end
 
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
   create_table "days", force: :cascade do |t|
     t.string "day"
     t.datetime "created_at", null: false
@@ -70,6 +79,16 @@ ActiveRecord::Schema.define(version: 2019_08_01_115006) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "time_alloted"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "search_suggestions", force: :cascade do |t|
@@ -215,6 +234,8 @@ ActiveRecord::Schema.define(version: 2019_08_01_115006) do
   add_foreign_key "booking_records", "spaces"
   add_foreign_key "booking_records", "users"
   add_foreign_key "member_subscriptions", "membership_plans"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "space_addresses", "spaces"
   add_foreign_key "space_availability_timings", "days"
   add_foreign_key "space_availability_timings", "spaces"
