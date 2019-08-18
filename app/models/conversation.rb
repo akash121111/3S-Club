ea72a584dfff7b2fedf64f9eafe167da56100064
Akash <akashkumar121111@gmail.com>
2019-08-18 22:10:30 +0530

    conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,8 +1,8 @@
 class Conversation < ApplicationRecord
 
     has_many :messages, dependent: :destroy
-  belongs_to :sender, foreign_key: :sender_id, class_name: User
-  belongs_to :recipient, foreign_key: :recipient_id, class_name: User
+  belongs_to :sender, foreign_key: :id, class_name: User
+  belongs_to :recipient, foreign_key: :id, class_name: User
 
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
